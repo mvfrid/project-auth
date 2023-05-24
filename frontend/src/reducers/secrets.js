0ba00d7296de88f0f2add52b004685284cc24f0d
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-05-24 15:16:47 +0200

    added secret reducers file

---
+++ frontend/src/reducers/secrets.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+// Här e vi osäkra om det ska vara items eller message
+    
+export const Secrets = createSlice({
+        username: "secrets", 
+        initialState: {
+         items: [],
+         error: null,
+        },
+    
+        reducers: {
+            setError: (store, action) => {
+                store.error = action.payload
+            },
+            setItems: (store, action) => {
+                store.items = action.payload
+            }
+
+        }
+    });
