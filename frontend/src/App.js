0f45bd523675f3738c9356df8e22995fdaaab535
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-05-24 14:51:27 +0200

    added one route frontpage
    Co-authored-by: Irupé Pozo Graviz <IrupePozoGraviz@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FrontPage } from './components/FrontPage'
 
 export const App = () => {
   return (
+    <BrowserRouter>
     <div className="main-wrapper">
-      <FrontPage />
+    <Routes>
+      <Route path='/FrontPage' element={<FrontPage/>}></Route>
+    </Routes>
     </div>
+    </BrowserRouter>
   );
 }
