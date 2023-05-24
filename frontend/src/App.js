3865c8fcc1e33f2b22b1fa9c2679d872f4cca85d
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-05-24 15:23:16 +0200

    imported to app reducers file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { FrontPage } from './components/FrontPage'
+import { FrontPage } from './components/FrontPage';
+import { user } from 'reducers/user';
+import { Secrets } from 'reducers/secrets';
+import { configureStore } from '@reduxjs/toolkit';
+
 
 export const App = () => {
+
+  const reducer = combineReducers({
+    user: user.reducer,
+    secrets: secrets.reducers
+  });
+
+  const store = configureStore({reducer})
+
   return (
     <BrowserRouter>
     <div className="main-wrapper">
