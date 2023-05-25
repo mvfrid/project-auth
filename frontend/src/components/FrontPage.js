0686f5ac3bc468b44fff50f7919ef35a76b1f4db
Frid Matilda <matilda.x.frid@scania.com>
2023-05-25 22:25:53 +0200

    matildas styling added

--- frontend/src/components/FrontPage.js
+++ frontend/src/components/FrontPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
 import Button from '@mui/material/Button';
+import keyicon from '../assets/icons8-password-key-90.png'
 
 export const FrontPage = () => {
     const navigate = useNavigate();
@@ -10,18 +11,17 @@ export const FrontPage = () => {
       }
 
     return (
-    <div className="main-container">
-        <div className="frontpage">
-            <p>Welcome to your secret diary</p>
-            <Button type="submit"
+    <div className="main-container frontpage">
+        <h2>The perfect place to store your deepest and darkest secrets</h2>
+        <img src={keyicon} alt="key" className="key-icon" />
+        <Button type="submit"
             onClick={onClickGoToSignIn}
             fullWidth
             variant="contained"
-            sx={{ mt: 3, mb: 2 }}
+            sx={{ mt: 3, mb: 2, maxWidth: 400 }}
             >
-                LOG IN
-            </Button>
-        </div>
+            LOG IN
+        </Button>
     </div>
     
     )
