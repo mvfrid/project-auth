738853ceea7e6c7a79b2feb4cf0179f9f7bc73c3
Frid Matilda <matilda.x.frid@scania.com>
2023-05-25 22:40:38 +0200

    fixed bug

--- frontend/src/utils/urls.js
+++ frontend/src/utils/urls.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://project-auth-3nbrs2gipa-lz.a.run.app/";
+const BASE_URL = "https://project-auth-3nbrs2gipa-lz.a.run.app";
 
 // const BASE_URL = "mongodb://127.0.0.1/project-auth";
 // const BASE_URL = "http://localhost:8080";
