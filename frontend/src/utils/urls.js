9a158354d6c22df4d63a8abbdfe65e226ae7998b
Frid Matilda <matilda.x.frid@scania.com>
2023-05-25 18:08:18 +0200

    changed url

--- frontend/src/utils/urls.js
+++ frontend/src/utils/urls.js
@@ -1,6 +1,7 @@
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://project-express-api-amd2viobia-nw.a.run.app";
 
 // const BASE_URL = "mongodb://127.0.0.1/project-auth";
+// const BASE_URL = "http://localhost:8080";
 // const BASE_URL = "http://localhost:8080";
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`
